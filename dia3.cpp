f9a402f6c2bf5ae19b817f705072de5afc0a2360
marcohinostroza <marco.alfoncio@itgranada.com>
2022-01-25 10:41:13 +0100

    vectores

--- dia3.cpp
+++ dia3.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 using namespace std;
 
-
+/*void mostrarArray (string array[]){
+  for(int i = 0 ; i < array ->size() ; i++){
+    cout << "Juegos" << i << " = " << array[i] << endl;
+  }
+}*/
 
 int main(){
+  int numJuegos = 10;
+  string juegos[numJuegos];
 
 
+  for(int i = 0 ; i < numJuegos ; i++){
+    string jue;
+    cout << "Dime un juego: ";
+    cin >> jue;
+    juegos[i] = jue;
+  }
 
+  for(int i = 0 ; i < numJuegos ; i++){
+    cout << "Juegos" << i << " = " <<  juegos[i] << endl;
+  }
+
+  juegos[4] = "Tetris";
+
+  //mostrarArray(juegos);
+
+  /*Deberes:
+  -Rellenar un vector con los 100 primeros numeros.
+  -Sumar de 4 en 4 los numeros y sumar en otro vector.
+  {0,1,2,3,4,5,6,7} -> {6,22,38,....}*/
   return 0;
 }
