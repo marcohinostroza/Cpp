bfcdd329ad999153c79e66759430a8e38ada4dfa
marcohinostroza <marco.alfoncio@itgranada.com>
2022-01-27 13:38:48 +0100

    arrays[]

--- dia3.cpp
+++ dia3.cpp
@@ -15,12 +15,23 @@ int porcinco(int x){
   return (x * 5);
 }
 
+void subirNota (int notas[3], int posicion, int nuevaNota){
+  notas[posicion] = nuevaNota;
+}
+
+void imprimirArray(int &array[20], int tama){
+  for(int i = 0; i < tama; i++){
+    cout << array[i] << endl;
+  }
+}
+
 int main(){
   int numJuegos = 100;
   string juegos[numJuegos];
   int num1 = 2;
   int num2 = 0;
   int multx5;
+  int notas[] = {8, 4, 9};
   /*cout << "¿Cuantos juegos quieres introducir?: ";
   cin >> numJuegos;
 
@@ -37,11 +48,23 @@ int main(){
     cout << "Juegos" << i << " = " <<  juegos[i] << endl;
   }*/
 
-  num2 = porcinco(num1);
+  /*num2 = porcinco(num1);
   cout << "El numero1 es: " << num1 << endl;
   cout << "El numero2 es: " << num2 << endl;
 
-  meMultiplicox5(num1);
+  meMultiplicox5(num1);*/
+
+  cout << "Notas original" << endl;
+
+  imprimirArray(notas, 3);
+
+  cout << "Subimos la nota" << endl;
+
+  subirNota(notas, 1, 9);
+
+  cout << "Nota tras subir la nota" << endl;
+
+  imprimirArray(notas, 3);
 
   //mostrarArray(juegos);
 
