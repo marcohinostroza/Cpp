231689947774f1476df2f132d768de180ccd4abd
marcohinostroza <marco.alfoncio@itgranada.com>
2022-03-10 10:32:07 +0100

    finalizado

--- examen1003.cpp
+++ examen1003.cpp
@@ -1,4 +1,8 @@
+//Marco Antonio Alfoncio Hinostroza
+
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
 class Persona{
@@ -26,6 +30,7 @@ class Persona{
         this->edad = edad;
   }
 
+    //metodos
     void posiblementeMayor(){
       if(this->edad>17){
         cout << "es mayor" << endl;
@@ -35,6 +40,17 @@ class Persona{
       }
     }
 
+    /*vector <int> suerte(){
+      vector <int> v;
+      for(int i = 0; i < 3; i++ ){
+        i = rand()%10+1;
+        v.push_back(i);
+        cout << v.push_back(i) << endl;
+    }
+    return v;
+  }*/
+
+    //creando gets
     string getNombre (){
       return this->nombre;
     }
@@ -52,6 +68,13 @@ class Persona{
       cout << "Me llamo " << this->nombre << ", " << "tengo " << this->edad << " años " << "y mi DNI es " << this->dni << endl;
     }
 
+    /*void imprimirVector(vector<double> v){
+      for(int i= 0; i < v.size(); i++){
+        cout << v[i] << endl;
+      }
+    }*/
+
+
 };
 
 
@@ -59,7 +82,9 @@ class Persona{
 
 
 int main(){
-
+/*srand(time(NULL));
+vector <int> main(3,rand()%10+1)
+vector <int> suerte;*/
 
 Persona principal;
 principal.imprimir();
@@ -69,8 +94,10 @@ Persona secundaria("Fulanito", "931294H", 15);
 secundaria.imprimir();
 secundaria.posiblementeMayor();
 
-
-
+/*suerte = main;
+suerte = suerte();
+imprimirVector(suerte);
+*/
 
 
 
